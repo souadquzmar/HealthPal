00c98e0afd3760e0afec3e684fd3894e0b844ab9
souad <souadquzmar13@gmail.com>
2025-11-04 22:22:31 +0200

    end call between doctor and patient

--- src/controllers/callController.js
+++ src/controllers/callController.js
@@ -52,3 +52,53 @@ export const startCall = async (req, res) => {
     return res.status(500).json({ message: "Internal server error." });
   }
 };
+
+export const endCall = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const userRole = req.user.role;
+    const userId = req.user.id;
+
+    const [calls] = await db.query(
+      `SELECT c.*, a.id AS consultation_id, d.user_id AS doctor_user_id, p.user_id AS patient_user_id
+       FROM consultation_calls c
+       JOIN appointments a ON c.consultation_id = a.id
+       JOIN doctors d ON a.doctor_id = d.id
+       JOIN patients p ON a.patient_id = p.id
+       WHERE c.id = ?`,
+      [id]
+    );
+
+    if(calls.length === 0)
+        return res.status(404).json({message:'There is no active call for this consultation.'});
+
+    const call = calls[0];
+
+    const isParticipant =
+      (userRole === "doctor" && call.doctor_user_id === userId) ||
+      (userRole === "patient" && call.patient_user_id === userId);
+
+    if (!isParticipant)
+      return res.status(403).json({message: "Access denied. you are not part of this consultation"});
+
+    if(call.status === 'ended')
+        return res.status(400).json({message:'This call is already ended.'});
+
+    await db.query(`update consultation_calls set status = "ended", ended_at = NOW() where id = ?`,[id]);
+
+    return res.status(200).json({
+        message:'Call ended successfully.',
+        data:{
+            id: call.id,
+            consultation_id: call.consultation_id,
+            started_by: call.started_by,
+            started_at: call.started_at,
+            ended_at: new Date(),
+            status: 'ended'
+        }
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Internal server error." });
+  }
+};
--- src/routes/callRoutes.js
+++ src/routes/callRoutes.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import { verifyRole, verifyToken } from '../middlewares/authMiddleware.js';
-import { startCall } from '../controllers/callController.js';
+import { endCall, startCall } from '../controllers/callController.js';
 
 const router = express.Router();
 
 router.post('/call/start',verifyToken,verifyRole(['doctor','patient']),startCall);
-
+router.patch('/call/end/:id',verifyToken,verifyRole(['doctor','patient']),endCall);
 export default router;
